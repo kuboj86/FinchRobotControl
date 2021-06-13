df90fda5dd66f247fdb5c3a784f2930142477fd1
Jason Kubo <kuboj86@gmail.com>
2021-06-12 22:32:03 -0400

    Main methods dones

---
+++ FinchRobotControl/DataRecorder.cs
@@ -0,0 +1,82 @@
+﻿using FinchAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinchRobotControl
+{
+    class DataRecorder
+    {
+        internal static int GetNumberOfDataPoints()
+        {
+            int numberOfDataPoints = 0;
+            Program.DisplayScreenHeader("Number of Data Points");
+
+            numberOfDataPoints = Validation.ValidateIntResponse("How many number of Data Points?\n");
+
+            Program.DisplayContinuePrompt();
+            return numberOfDataPoints;
+
+        }
+        internal static double GetDataPointFrequency()
+        {
+            double dataPointFrequency;
+            Program.DisplayScreenHeader("Data Point Frequency");
+
+            dataPointFrequency = Validation.ValidateDoubleResponse("Frequency of Data Points?\n");
+
+            Program.DisplayContinuePrompt();
+            return dataPointFrequency;
+        }
+
+        internal static double[] GetDataRecorderData(int numberOfDataPoints, double dataPointFrequency, Finch finchRobot)
+        {
+            double[] temperatures = new double[numberOfDataPoints];
+
+            Program.DisplayScreenHeader("Get Data");
+
+            Console.WriteLine($"Number of Data Points: {numberOfDataPoints}");
+            Console.WriteLine($"Data Point Frequency: {dataPointFrequency}\n");
+            Console.WriteLine("Starting temperature recording\n");
+
+            int awaitTime = (int)(dataPointFrequency * 100);
+
+            for (int i = 0; i < numberOfDataPoints; i++)
+            {
+                temperatures[i] = finchRobot.getTemperature();
+                Console.WriteLine($"Reading {i + 1}: {temperatures[i].ToString("n2")}");
+                finchRobot.wait(awaitTime);
+            }
+
+            Program.DisplayContinuePrompt();
+
+            return temperatures;
+        }
+
+        internal static void DisplayData(double[] temperatures)
+        {
+            Program.DisplayScreenHeader("Display Data");
+            DisplayDataTable(temperatures);
+            Program.DisplayContinuePrompt();
+
+        }
+        internal static void DisplayDataTable(double[] temperatures)
+        {
+            Console.WriteLine(
+                "Recording #".PadLeft(10) +
+                "Temp".PadLeft(10));
+            Console.WriteLine(
+                "___________".PadLeft(10) +
+                "___________".PadLeft(10));
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                Console.WriteLine(
+                    (i + 1).ToString().PadLeft(10) +
+                    temperatures[i].ToString("n2").PadLeft(10));
+            }
+        }
+    }
+}
---
+++ FinchRobotControl/Validation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinchRobotControl
+{
+    public class Validation
+    {
+        public static int ValidateIntResponse(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            int selection;
+            if (int.TryParse(input, out selection))
+            {
+                return selection;
+            }
+            else
+            {
+                return ValidateIntResponse("I'm sorry I didn't quite get that answer.\n");
+            }
+        }
+        public static double ValidateDoubleResponse(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            double selection;
+            if (double.TryParse(input, out selection))
+            {
+                return selection;
+            }
+            else
+            {
+                return ValidateDoubleResponse("I'm sorry I didn't quite get that answer.\n");
+            }
+        }
+
+        public static string ValidateYesNo(string message)
+        {
+            bool isValid = true;
+            string answer = "";
+            while (isValid)
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine().ToLower();
+                if (input == "y")
+                {
+                    isValid = false;
+                    answer = "yes";
+                }
+                else if (input == "n")
+                {
+                    isValid = false;
+                }
+                else
+                {
+                    isValid = true;
+                    ValidateYesNo("Im sorry I didn't quite understand your response.\n");
+                }
+            }
+            return answer;
+        }
+    }
+}
